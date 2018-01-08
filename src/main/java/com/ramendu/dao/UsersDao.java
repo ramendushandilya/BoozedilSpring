007c303fe3002c930181f78fa9288015780f7c7e
ramendushandilya <ramendu.shandilya@gmail.com>
2018-01-08 18:36:40 +0530

    commit

---
+++ src/main/java/com/ramendu/dao/UsersDao.java
@@ -0,0 +1,14 @@
+package com.ramendu.dao;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author rams0516
+ *         Date: 1/8/2018
+ *         Time: 6:21 PM
+ */
+
+@Component
+public class UsersDao {
+
+}
